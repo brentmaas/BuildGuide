949842fc619aaa3b33714e3da26d269fcce33ff2
Brent Maas <brent.maas@hotmail.com>
2021-05-09 13:11:25 +0200

    New logo (also swapped some dome modes)

--- src/main/java/brentmaas/buildguide/shapes/ShapeSphere.java
+++ src/main/java/brentmaas/buildguide/shapes/ShapeSphere.java
@@ -9,15 +9,15 @@ import net.minecraft.util.text.TranslationTextComponent;
 public class ShapeSphere extends Shape{
 	private enum dome {
 		NO,
-		NEGATIVE_X,
-		NEGATIVE_Y,
-		NEGATIVE_Z,
 		POSITIVE_X,
 		POSITIVE_Y,
-		POSITIVE_Z
+		POSITIVE_Z,
+		NEGATIVE_X,
+		NEGATIVE_Y,
+		NEGATIVE_Z
 	}
 	
-	private String[] domeNames = {"-", "-X", "-Y", "-Z", "+X", "+Y", "+Z"};
+	private String[] domeNames = {"-", "+X", "+Y", "+Z", "-X", "-Y", "-Z"};
 	
 	private PropertyPositiveInt propertyRadius = new PropertyPositiveInt(0, 145, 3, new TranslationTextComponent("property.buildguide.radius"), () -> {this.update();});
 	private PropertyEnum<dome> propertyDome = new PropertyEnum<dome>(0, 165, dome.NO, new TranslationTextComponent("property.buildguide.dome"), () -> {this.update();}, domeNames);
