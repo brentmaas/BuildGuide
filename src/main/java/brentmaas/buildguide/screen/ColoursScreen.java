db005b8356e9eddeec65705e214f5c50303e7738
Brent Maas <brent.maas@hotmail.com>
2021-07-31 18:10:16 +0200

    Fix bug where Set in colours would set both + use own Slider

--- src/main/java/brentmaas/buildguide/screen/ColoursScreen.java
+++ src/main/java/brentmaas/buildguide/screen/ColoursScreen.java
@@ -6,10 +6,11 @@ import brentmaas.buildguide.BuildGuide;
 import brentmaas.buildguide.State;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.AbstractSlider;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.fml.client.gui.widget.Slider;
 
 public class ColoursScreen extends Screen{
 	private String titleShape;
@@ -38,23 +39,37 @@ public class ColoursScreen extends Screen{
 		titleShape = new TranslationTextComponent("screen.buildguide.shape").getString();
 		titleBasepos = new TranslationTextComponent("screen.buildguide.basepos").getString();
 		
-		sliderShapeR = new Slider(0, 40, 100, 20, new StringTextComponent("R: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourShapeR, true, true, null);
-		sliderShapeG = new Slider(0, 60, 100, 20, new StringTextComponent("G: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourShapeG, true, true, null);
-		sliderShapeB = new Slider(0, 80, 100, 20, new StringTextComponent("B: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourShapeB, true, true, null);
-		sliderShapeA = new Slider(0, 100, 100, 20, new StringTextComponent("A: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourShapeA, true, true, null);
-		sliderBaseposR = new Slider(120, 40, 100, 20, new StringTextComponent("R: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourBaseposR, true, true, null);
-		sliderBaseposG = new Slider(120, 60, 100, 20, new StringTextComponent("G: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourBaseposG, true, true, null);
-		sliderBaseposB = new Slider(120, 80, 100, 20, new StringTextComponent("B: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourBaseposB, true, true, null);
-		sliderBaseposA = new Slider(120, 100, 100, 20, new StringTextComponent("A: "), new StringTextComponent(""), 0.0, 1.0, BuildGuide.state.colourBaseposA, true, true, null);
+		sliderShapeR = new Slider(0, 40, 100, 20, new StringTextComponent("R: "), 0.0, 1.0, BuildGuide.state.colourShapeR);
+		sliderShapeG = new Slider(0, 60, 100, 20, new StringTextComponent("G: "), 0.0, 1.0, BuildGuide.state.colourShapeG);
+		sliderShapeB = new Slider(0, 80, 100, 20, new StringTextComponent("B: "), 0.0, 1.0, BuildGuide.state.colourShapeB);
+		sliderShapeA = new Slider(0, 100, 100, 20, new StringTextComponent("A: "), 0.0, 1.0, BuildGuide.state.colourShapeA);
+		sliderBaseposR = new Slider(120, 40, 100, 20, new StringTextComponent("R: "), 0.0, 1.0, BuildGuide.state.colourBaseposR);
+		sliderBaseposG = new Slider(120, 60, 100, 20, new StringTextComponent("G: "), 0.0, 1.0, BuildGuide.state.colourBaseposG);
+		sliderBaseposB = new Slider(120, 80, 100, 20, new StringTextComponent("B: "), 0.0, 1.0, BuildGuide.state.colourBaseposB);
+		sliderBaseposA = new Slider(120, 100, 100, 20, new StringTextComponent("A: "), 0.0, 1.0, BuildGuide.state.colourBaseposA);
 		
-		buttonSetShape = new Button(0, 120, 100, 20, new TranslationTextComponent("screen.buildguide.set"), button -> State.updateCurrentShape());
-		buttonSetBasepos = new Button(120, 120, 100, 20, new TranslationTextComponent("screen.buildguide.set"), button -> State.updateCurrentShape());
+		buttonSetShape = new Button(0, 120, 100, 20, new TranslationTextComponent("screen.buildguide.set"), button -> {
+			BuildGuide.state.colourShapeR = (float) sliderShapeR.getValue();
+			BuildGuide.state.colourShapeG = (float) sliderShapeG.getValue();
+			BuildGuide.state.colourShapeB = (float) sliderShapeB.getValue();
+			BuildGuide.state.colourShapeA = (float) sliderShapeA.getValue();
+			
+			State.updateCurrentShape();
+		});
+		buttonSetBasepos = new Button(120, 120, 100, 20, new TranslationTextComponent("screen.buildguide.set"), button -> {
+			BuildGuide.state.colourBaseposR = (float) sliderBaseposR.getValue();
+			BuildGuide.state.colourBaseposG = (float) sliderBaseposG.getValue();
+			BuildGuide.state.colourBaseposB = (float) sliderBaseposB.getValue();
+			BuildGuide.state.colourBaseposA = (float) sliderBaseposA.getValue();
+			
+			State.updateCurrentShape();
+		});
 		
 		buttonDefaultShape = new Button(0, 140, 100, 20, new TranslationTextComponent("screen.buildguide.default"), button -> {
-			sliderShapeR.setValue(1.0);
-			sliderShapeG.setValue(1.0);
-			sliderShapeB.setValue(1.0);
-			sliderShapeA.setValue(0.5);
+			sliderShapeR.setManualValue(1.0);
+			sliderShapeG.setManualValue(1.0);
+			sliderShapeB.setManualValue(1.0);
+			sliderShapeA.setManualValue(0.5);
 			sliderShapeR.updateSlider();
 			sliderShapeG.updateSlider();
 			sliderShapeB.updateSlider();
@@ -66,10 +81,10 @@ public class ColoursScreen extends Screen{
 			State.updateCurrentShape();
 		});
 		buttonDefaultBasepos = new Button(120, 140, 100, 20, new TranslationTextComponent("screen.buildguide.default"), button -> {
-			sliderBaseposR.setValue(1.0);
-			sliderBaseposG.setValue(0.0);
-			sliderBaseposB.setValue(0.0);
-			sliderBaseposA.setValue(0.5);
+			sliderBaseposR.setManualValue(1.0);
+			sliderBaseposG.setManualValue(0.0);
+			sliderBaseposB.setManualValue(0.0);
+			sliderBaseposA.setManualValue(0.5);
 			sliderBaseposR.updateSlider();
 			sliderBaseposG.updateSlider();
 			sliderBaseposB.updateSlider();
@@ -107,28 +122,37 @@ public class ColoursScreen extends Screen{
 		font.drawStringWithShadow(matrixStack, title.getString(), (width - font.getStringWidth(title.getString())) / 2, 5, 0xFFFFFF);
 		font.drawStringWithShadow(matrixStack, titleShape, (100 - font.getStringWidth(titleShape)) / 2, 25, 0xFFFFFF);
 		font.drawStringWithShadow(matrixStack, titleBasepos, 120 + (100 - font.getStringWidth(titleBasepos)) / 2, 25, 0xFFFFFF);
-		
-		BuildGuide.state.colourShapeR = (float) sliderShapeR.getValue();
-		BuildGuide.state.colourShapeG = (float) sliderShapeG.getValue();
-		BuildGuide.state.colourShapeB = (float) sliderShapeB.getValue();
-		BuildGuide.state.colourShapeA = (float) sliderShapeA.getValue();
-		BuildGuide.state.colourBaseposR = (float) sliderBaseposR.getValue();
-		BuildGuide.state.colourBaseposG = (float) sliderBaseposG.getValue();
-		BuildGuide.state.colourBaseposB = (float) sliderBaseposB.getValue();
-		BuildGuide.state.colourBaseposA = (float) sliderBaseposA.getValue();
 	}
 	
-	@Override
-	public void onClose() {
-		BuildGuide.state.colourShapeR = (float) sliderShapeR.getValue();
-		BuildGuide.state.colourShapeG = (float) sliderShapeG.getValue();
-		BuildGuide.state.colourShapeB = (float) sliderShapeB.getValue();
-		BuildGuide.state.colourShapeA = (float) sliderShapeA.getValue();
-		BuildGuide.state.colourBaseposR = (float) sliderBaseposR.getValue();
-		BuildGuide.state.colourBaseposG = (float) sliderBaseposG.getValue();
-		BuildGuide.state.colourBaseposB = (float) sliderBaseposB.getValue();
-		BuildGuide.state.colourBaseposA = (float) sliderBaseposA.getValue();
+	class Slider extends AbstractSlider {
+		private double min, max;
+		private String prefix;
 		
-		super.onClose();
+		public Slider(int x, int y, int width, int height, TextComponent name, double min, double max, double value) {
+			super(x, y, width, height, new StringTextComponent(name.getString() + ": " + Math.round(10.0 * value) / 10.0), (value - min) / (max - min));
+			this.min = min;
+			this.max = max;
+			prefix = name.getString() + ": ";
+		}
+		
+		protected void func_230979_b_() { //updateMessage
+			setMessage(new StringTextComponent(prefix + Math.round(10.0 * getValue()) / 10.0));
+		}
+		
+		protected void func_230972_a_() { //applyValue
+			
+		}
+		
+		public void updateSlider() {
+			func_230979_b_();
+		}
+		
+		public void setManualValue(double value) {
+			this.sliderValue = (value - min) / (max - min);
+		}
+		
+		public double getValue() {
+			return sliderValue * (max - min) + min;
+		}
 	}
 }
