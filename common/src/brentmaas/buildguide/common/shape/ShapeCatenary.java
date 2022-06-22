f45104520c2eff9389805368dc8ec88b826e9504
Brent Maas <brent.maas@hotmail.com>
2022-06-22 18:29:29 +0200

    Invertable catenary

--- common/src/brentmaas/buildguide/common/shape/ShapeCatenary.java
+++ common/src/brentmaas/buildguide/common/shape/ShapeCatenary.java
@@ -1,6 +1,7 @@
 package brentmaas.buildguide.common.shape;
 
 import brentmaas.buildguide.common.BuildGuide;
+import brentmaas.buildguide.common.property.PropertyBoolean;
 import brentmaas.buildguide.common.property.PropertyInt;
 import brentmaas.buildguide.common.property.PropertyPositiveInt;
 import brentmaas.buildguide.common.property.PropertyRunnable;
@@ -19,6 +20,7 @@ public class ShapeCatenary extends Shape {
 		update();
 	}, BuildGuide.screenHandler.translate("property.buildguide.setendpoint"));
 	private PropertyPositiveInt propertyAddLength = new PropertyPositiveInt(1, BuildGuide.screenHandler.translate("property.buildguide.addlength"), () -> update());
+	private PropertyBoolean propertyInvert = new PropertyBoolean(false, BuildGuide.screenHandler.translate("property.buildguide.invert"), () -> update());
 	
 	public ShapeCatenary() {
 		super();
@@ -28,12 +30,14 @@ public class ShapeCatenary extends Shape {
 		properties.add(propertyDz);
 		properties.add(propertySetEndpoint);
 		properties.add(propertyAddLength);
+		properties.add(propertyInvert);
 	}
 	
 	protected void updateShape(IShapeBuffer buffer) throws Exception {
 		double dr = Math.sqrt(propertyDx.value * propertyDx.value + propertyDz.value * propertyDz.value);
 		int dy = propertyDy.value;
 		int ds = propertyAddLength.value;
+		boolean inv = propertyInvert.value;
 		if(dr >= 1) {
 			double s = Math.sqrt(dr * dr + dy * dy) + ds;
 			
@@ -57,8 +61,9 @@ public class ShapeCatenary extends Shape {
 			double dx = ((double) propertyDx.value) / d;
 			double dz = ((double) propertyDz.value) / d;
 			double fac = Math.sqrt(dx * dx + dz * dz);
+			int h = (int) (Math.min(0, dy) - a * Math.cosh(rl / a));
 			for(int i = 0;i <= d;++i) {
-				for(int j = (int) (Math.min(0, dy) - a * Math.cosh(rl / a));j <= Math.max(0, dy);++j) {
+				for(int j = h;j <= Math.max(0, dy);++j) {
 					//Dirty hacks so the ends are actually at (0,0,0) and (dx,dy,dz)
 					if(i == 0 && j > 0) continue;
 					if(i == d && j > dy) continue;
@@ -66,7 +71,7 @@ public class ShapeCatenary extends Shape {
 					if((a * Math.cosh((i * fac + rl) / a) - a * Math.cosh(rl / a) >= j - 0.5 && a * Math.cosh((i * fac + rl) / a) - a * Math.cosh(rl / a) < j + 0.5) //Check down
 					|| (a * Math.cosh(((i - 0.5) * fac + rl) / a) - a * Math.cosh(rl / a) >= j && a * Math.cosh(((i + 0.5) * fac + rl) / a) - a * Math.cosh(rl / a) < j) //Check left
 					|| (a * Math.cosh(((i + 0.5) * fac + rl) / a) - a * Math.cosh(rl / a) >= j && a * Math.cosh(((i - 0.5) * fac + rl) / a) - a * Math.cosh(rl / a) < j)) { //Check right
-						addShapeCube(buffer, (int) (dx * i + Math.signum(dx) * 0.5), j, (int) (dz * i + Math.signum(dz) * 0.5));
+						addShapeCube(buffer, inv ? (int) (dx * (d - i) + Math.signum(dx) * 0.5) : (int) (dx * i + Math.signum(dx) * 0.5), inv ? dy - j : j, inv ? (int) (dz * (d - i) + Math.signum(dz) * 0.5) : (int) (dz * i + Math.signum(dz) * 0.5));
 					}
 				}
 			}
