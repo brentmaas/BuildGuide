6b78e5c95a4e27ef395523be8b734499698556f7
Brent Maas <brent.maas@hotmail.com>
2022-11-06 16:14:03 +0100

    1.19 backport

---
+++ fabric1.19/src/main/java/brentmaas/buildguide/fabric/screen/widget/TextFieldImpl.java
@@ -0,0 +1,34 @@
+package brentmaas.buildguide.fabric.screen.widget;
+
+import brentmaas.buildguide.common.screen.widget.ITextField;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.text.Text;
+
+public class TextFieldImpl extends TextFieldWidget implements ITextField {
+	
+	
+	public TextFieldImpl(int x, int y, int width, int height, String value) {
+		super(MinecraftClient.getInstance().textRenderer, x, y, width, height, Text.literal(value));
+	}
+	
+	public void setTextValue(String text) {
+		setText(text);
+	}
+	
+	public void setTextColour(int colour) {
+		setEditableColor(colour);
+	}
+	
+	public void setVisibility(boolean visible) {
+		setVisible(visible);
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String getTextValue() {
+		return getText();
+	}
+}
---
+++ forge1.19/src/main/java/brentmaas/buildguide/forge/screen/widget/ButtonImpl.java
@@ -0,0 +1,25 @@
+package brentmaas.buildguide.forge.screen.widget;
+
+import brentmaas.buildguide.common.screen.widget.IButton;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
+
+public class ButtonImpl extends Button implements IButton {
+	
+	
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public ButtonImpl(int x, int y, int width, int height, String text, IButton.IPressable onPress) {
+		super(x, y, width, height, Component.literal(text), button -> onPress.onPress());
+	}
+}
