c85676c7b01f1df11a10403caed98e0f70107343
Brent Maas <brent.maas@hotmail.com>
2025-01-15 15:55:27 +0100

    Fix deprecation warnings

--- forge1.19.4/src/main/java/brentmaas/buildguide/forge/BuildGuideForge.java
+++ forge1.19.4/src/main/java/brentmaas/buildguide/forge/BuildGuideForge.java
@@ -13,16 +13,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
 
 @Mod(BuildGuide.modid)
 public class BuildGuideForge {
 	private static final Logger logger = LogManager.getLogger();
 	
-	public BuildGuideForge() {
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	public BuildGuideForge(FMLJavaModLoadingContext context) {
+		context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
 			BuildGuide.register(new InputHandler(), new ScreenHandler(), new WidgetHandler(), new StateManager(), new ShapeHandler(), new RenderHandler(), new LogHandler(logger), new File(Minecraft.getInstance().gameDirectory, "config"));
 		});
